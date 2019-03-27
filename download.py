98afcc1adbd5d410b79912c95f4e6ace7338acf5
Haroon Chughtai <haroon.chughtai@nhs.net>
2019-03-27 20:57:53 +0000

    figuring out filter

--- download.py
+++ download.py
@@ -1,25 +1,32 @@
 import click
 import os
-from functools import reduce
 import pdfx
-import json
+import re
 
 @click.command()
-@click.argument("report")
+@click.argument('report', type=click.Path(exists=True))
 def download(report):
-    
-    # check if the file pointed to exists
-    if os.path.isfile(report) == False:
-        click.echo("Invalid file %s" % report)    
-        return
     
     # extract links from pdf
     pdf = pdfx.PDFx(report)
     references_dict = pdf.get_references_as_dict()
-    
-    with open('data/result.json', 'w') as fp:
-        json.dump(references_dict, fp)
 
+    merged_data = [value for (key, value) in (references_dict.items())][0]
+
+    filtered_data = filter(check_file_url, merged_data)
+
+    import pdb; pdb.set_trace()
+
+def check_file_url(url_path):
+    
+    expr = p = re.compile('(?=\w+\.\w{3,4}$).+')
+    
+    is_file_url = expr.match(url_path)
+    
+    if is_file_url:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     download()
