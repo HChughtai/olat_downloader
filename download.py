9acd3a6f3733de9abd8aa6f315d864b9ffccbb6d
Haroon Chughtai <haroon.chughtai@nhs.net>
2019-03-27 21:12:03 +0000

    file extraction works

--- download.py
+++ download.py
@@ -11,15 +11,15 @@ def download(report):
     pdf = pdfx.PDFx(report)
     references_dict = pdf.get_references_as_dict()
 
-    merged_data = [value for (key, value) in (references_dict.items())][0]
-
+    data = [value for (key, value) in (references_dict.items())]
+    merged_data = [j for i in data for j in i]
     filtered_data = filter(check_file_url, merged_data)
 
-    import pdb; pdb.set_trace()
+    #import pdb; pdb.set_trace()
 
 def check_file_url(url_path):
     
-    expr = p = re.compile('(?=\w+\.\w{3,4}$).+')
+    expr = p = re.compile('.*\.\w{3,4}$')
     
     is_file_url = expr.match(url_path)
     
